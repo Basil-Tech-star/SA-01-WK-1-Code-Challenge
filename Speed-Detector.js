35ac3c88e763e4636b822b629b91d964c9435c53
Basil Wabuke <babazwabuke4@gmail.com>
2024-10-14 14:27:41 +0300

    tenth commit

--- Speed-Detector.js
+++ Speed-Detector.js
@@ -3,23 +3,23 @@ function detectSpeed(speed) {// fucntion declared to detect speed and calculate 
     let demeritPoints = 0;//initializing demerit points to 0
     const demeritPointMax = 12;//maximum allowable demeritpoints declared before license is suspended
     if (speed < speedLimit) {// if speed is below the speed limit
-        console.log('Ok')// if speed is within limit log 'ok'
+        return 'Ok'// if speed is within limit log 'ok'
         
     } else {
         const excessSpeed = speed - speedLimit;// calculating the excess poitns over the speedlimit
         demeritPoints = Math.floor(excessSpeed / 5);//calculating the demeritpoints for every 5km/h over the speedlimit
-
-        console.log(`Points:${demeritPoints}`);// log calculated demerit points
-
-        if (demeritPoints > demeritPointMax) { // if demeritpoints is above the allowable demeritpoints maximum
+        
+        //check if demerit points exceed the maximum
+        if (demeritPoints > demeritPointMax) { 
             console.log("License suspended");// log 'license suspended' if demeiritpoints exceed the maximum
             
-        
-    }
+        }
+        return `Points:${demeritPoints}`;// log calculated demerit points
     
-}
+} 
+
 
 }
 const carSpeed = parseFloat(prompt("Enter the speed of the car:"));//prompt the  user to enter speed of the car and convert to float
-detectSpeed(carSpeed);// call the function to detect speed after the user's input
+alert(detectSpeed(carSpeed));// call the function to detect speed after the user's input
 
